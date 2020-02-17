f6ccaacca8f0b5a79f0e9e89711e46fdf8422292
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-17 23:05:24 +0100

    Adapt the usage message.

--- src/poetry_merge_lock/console.py
+++ src/poetry_merge_lock/console.py
@@ -7,4 +7,4 @@ from . import __version__
 @click.command()
 @click.version_option(version=__version__)
 def main() -> None:
-    """poetry-merge-lock."""
+    """Merge the lock file of a Poetry project."""
