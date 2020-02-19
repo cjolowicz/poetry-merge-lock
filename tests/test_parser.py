06e3c8c920876805628ca2b97adb64cf6b0ee444
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-19 10:27:16 +0100

    Fix TYP201 (Missing return type annotation for public function)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,18 +3,18 @@ import pytest
 from poetry_merge_lock import parser
 
 
-def test_tokenize_empty_string():
+def test_tokenize_empty_string() -> None:
     """The empty string produces a default token."""
     assert parser.Token.DEFAULT == parser.tokenize("")
 
 
-def test_tokenize_conflict_start():
+def test_tokenize_conflict_start() -> None:
     """The start marker of a merge conflict produces the corresponding token."""
     line = "<<<<<<< HEAD\n"
     assert parser.Token.CONFLICT_START == parser.tokenize(line)
 
 
-def test_parse_expected_token():
+def test_parse_expected_token() -> None:
     """The separator of a merge conflict switches to THEIRS state."""
     line = "=======\n"
     assert (parser.Token.CONFLICT_SEPARATOR, parser.State.THEIRS) == parser.parse_line(
@@ -22,7 +22,7 @@ def test_parse_expected_token():
     )
 
 
-def test_parse_unexpected_token():
+def test_parse_unexpected_token() -> None:
     """The separator of a merge conflict cannot occur in COMMON state."""
     line = "=======\n"
     with pytest.raises(parser.UnexpectedTokenError):
