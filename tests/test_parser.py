bbe31481e2bda86c1cea7c888d5a6e304f1203e4
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-19 10:25:47 +0100

    Fix D100 and D103 (Missing docstrings in public module and function)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,18 +1,22 @@
+"""Parser tests."""
 from poetry_merge_lock import parser
 
 import pytest
 
 
 def test_tokenize_empty_string():
+    """The empty string produces a default token."""
     assert parser.Token.DEFAULT == parser.tokenize("")
 
 
 def test_tokenize_conflict_start():
+    """The start marker of a merge conflict produces the corresponding token."""
     line = "<<<<<<< HEAD\n"
     assert parser.Token.CONFLICT_START == parser.tokenize(line)
 
 
 def test_parse_expected_token():
+    """The separator of a merge conflict switches to THEIRS state."""
     line = "=======\n"
     assert (parser.Token.CONFLICT_SEPARATOR, parser.State.THEIRS) == parser.parse_line(
         line, parser.State.OURS
@@ -20,6 +24,7 @@ def test_parse_expected_token():
 
 
 def test_parse_unexpected_token():
+    """The separator of a merge conflict cannot occur in COMMON state."""
     line = "=======\n"
     with pytest.raises(parser.UnexpectedTokenError):
         parser.parse_line(line, parser.State.COMMON)
