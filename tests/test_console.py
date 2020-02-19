9fb7c34eb8477d569f2e21c9e4b4526eb38da985
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-19 23:50:08 +0100

    Add test case

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,4 +1,6 @@
 """Test cases for the console module."""
+import re
+
 from click.testing import CliRunner
 import pytest
 
@@ -15,3 +17,10 @@ def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_prints_content_hash(runner: CliRunner) -> None:
+    """It prints the content hash."""
+    result = runner.invoke(console.main, ["--print-content-hash"])
+    pattern = re.compile("[0-9a-f]{64}\n")
+    assert pattern.match(result.output) is not None
