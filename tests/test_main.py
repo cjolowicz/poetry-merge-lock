a32c1d68c6dd24e0933d231d672390b0196ff9c8
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-13 07:00:45 +0200

    Add test case for --print-content-hash

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,9 @@ def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
+
+
+def test_main_succeeds_with_print_content_hash(runner: CliRunner) -> None:
+    """It exits with a status code of zero."""
+    result = runner.invoke(__main__.main, ["--print-content-hash"])
+    assert result.exit_code == 0
