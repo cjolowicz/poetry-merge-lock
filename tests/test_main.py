d9b569153e05dd8167a8291167591dc9b9a83c2e
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-05-13 07:43:35 +0200

    Upgrade cookiecutter template (#52)
    * Update template
    
    * Reorder imports
    
    * Add test case for --print-content-hash

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,6 @@
 """Test cases for the __main__ module."""
-import re
-
-from click.testing import CliRunner
 import pytest
+from click.testing import CliRunner
 
 from poetry_merge_lock import __main__
 
@@ -19,8 +17,7 @@ def test_main_succeeds(runner: CliRunner) -> None:
     assert result.exit_code == 0
 
 
-def test_main_prints_content_hash(runner: CliRunner) -> None:
-    """It prints the content hash."""
+def test_main_succeeds_with_print_content_hash(runner: CliRunner) -> None:
+    """It exits with a status code of zero."""
     result = runner.invoke(__main__.main, ["--print-content-hash"])
-    pattern = re.compile("[0-9a-f]{64}\n")
-    assert pattern.match(result.output) is not None
+    assert result.exit_code == 0
